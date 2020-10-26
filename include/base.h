cf19617da06a09178fecc22d4474d6ab1567fc31
520Matches <HengDi_Sun@163.com>
2020-10-26 21:59:31 +0800

    add gpio uart

---
+++ include/base.h
@@ -0,0 +1,6 @@
+#ifndef _BASE_H
+#define _BASE_H
+
+#define BASE_ADDRESS 0x3F000000
+
+#endif
---
+++ include/gpio.h
@@ -0,0 +1,10 @@
+#ifndef _GPIO_H
+#define _GPIO_H
+
+#include "base.h"
+
+#define GPFSEL1 (BASE_ADDRESS + 0x200004)
+#define GPPUD (BASE_ADDRESS + 0x200094)
+#define GPPUDCLK0 (BASE_ADDRESS + 0x2000098)
+
+#endif
